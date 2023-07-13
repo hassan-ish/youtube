c4ece57ca46af9338f542694beb4772389b6a20b
hassan-ish <hassan.abuishaiba@gmail.com>
2023-07-13 21:26:10 +0300

    controled form for search term

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const navigate = useNavigate();
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${searchTerm}`);
+    setSearchTerm("");
+  };
+  return (
+    <Paper
+      component="form"
+      onSubmit={handelSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        type="text"
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
